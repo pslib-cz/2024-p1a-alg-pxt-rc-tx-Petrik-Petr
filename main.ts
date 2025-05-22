c4047bff736cbad6b7eba7112c0a0ece0bbf193e
bomboklada <radovan.lahoda.024@pslib.cz>
2025-05-22 21:23:45 +0200

    Zavody main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,25 @@
 radio.setGroup(123)
+radio.setFrequencyBand(48)
 radio.setTransmitSerialNumber(true)
-control.deviceSerialNumber()
- 
+let packet : string
+type zolik = {
+    x: Number,
+    y: Number
+}
+let gril = 0
+let f = 0
+let kolik: zolik = {x:0, y:0}
+let zprava: string
+let zprva: string
+let tohle = control.deviceSerialNumber().toString()
+
 basic.forever(function () {
-    let X = input.acceleration(Dimension.X)
-    let Y = input.acceleration(Dimension.Y)
-    radio.sendValue('X',X)
+    kolik.x = input.acceleration(Dimension.X)
+    kolik.y = input.acceleration(Dimension.Y)
+    zprva = convertToText((kolik.x))
+    zprava = convertToText((kolik.y))
     basic.pause(10)
-    radio.sendValue('Y',Y)
+    radio.sendString(tohle +','+ zprava +','+ zprva)
     basic.pause(10)
 })
+ 
